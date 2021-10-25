354a3687468e3f273cacf043c60c2cf1c4bad13e
JohnBrown2022 <92580794+JohnBrown2022@users.noreply.github.com>
2021-10-25 17:42:35 +0800

    Update keyantongqd.py

--- keyantongqd.py
+++ keyantongqd.py
@@ -3,34 +3,35 @@
 
 # In[70]:
 
-
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.action_chains import ActionChains
-
+from selenium.webdriver.chrome.options import Options
+import os
 
 def checkin(username, password):
-    try:
-        #登录
-        daka = "https://www.ablesci.com/site/login"
-        driver = webdriver.Chrome()
-        driver.get(daka)
-        driver.find_element_by_id("username").send_keys("{}").format(username)
-        driver.find_element_by_id("password").send_keys("{}").format(password)
-        driver.find_element_by_xpath("/html/body/div[4]/div/div/form/div/div[1]/div/div/div/div/div/div[4]/button").click()
+#     try:
+    #登录
+    daka = "https://www.ablesci.com/site/login"
+    driver = webdriver.Chrome()
+    driver.get(daka)
+    driver.find_element_by_id("username").send_keys("{}").format(username)
+    driver.find_element_by_id("password").send_keys("{}").format(password)
+    driver.find_element_by_xpath("/html/body/div[4]/div/div/form/div/div[1]/div/div/div/div/div/div[4]/button").click()
 
-        sleep(3)
+    sleep(3)
 
-        element=driver.find_element_by_xpath("/html/body/div[2]/div/ul[2]/li[1]/button")
-        element.click()
+    element=driver.find_element_by_xpath("/html/body/div[2]/div/ul[2]/li[1]/button")
+    element.click()
 
-        #关闭浏览器
-        sleep(3)
-        driver.quit()
-    except Exception as e:
-    pass
+    #关闭浏览器
+    sleep(3)
+    driver.quit()
+#     except Exception as e:
+#     pass
 
-checkin("1846128548@qq.com", "calman.qzy.")
+checkin(username = os.environ["USERNAME"],
+        password = os.environ["PASSWORD"])
 
 
 # In[ ]:
