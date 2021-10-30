0dff458173bb668143ac2615217d1555f9654746
JohnBrown2022 <92580794+JohnBrown2022@users.noreply.github.com>
2021-10-30 20:20:25 +0800

    Update keyantongqd.py

--- keyantongqd.py
+++ keyantongqd.py
@@ -10,27 +10,29 @@ from selenium.webdriver.chrome.options import Options
 import os
 
 def checkin(username, password):
-    try:
+#     try:
         
-        daka = "https://www.ablesci.com/site/login"
-        driver = webdriver.Chrome()
-        driver.get(daka)
-        driver.find_element_by_id("LAY-user-login-email").send_keys("{}".format(username))
-        driver.find_element_by_id("LAY-user-login-password").send_keys("{}".format(password))
-        driver.find_element_by_xpath("/html/body/div[4]/div/div/form/div/div[1]/div/div/div/div/div/div[4]/button").click()
+    daka = "https://www.ablesci.com/site/login"
+    driver = webdriver.Chrome()
+    driver.get(daka)
+    
+    sleep(3)
+    driver.find_element_by_id("LAY-user-login-email").send_keys("{}".format(username))
+    driver.find_element_by_id("LAY-user-login-password").send_keys("{}".format(password))
+    driver.find_element_by_xpath("/html/body/div[4]/div/div/form/div/div[1]/div/div/div/div/div/div[4]/button").click()
 
-        sleep(3)
+    sleep(3)
 
-        element=driver.find_element_by_xpath("/html/body/div[2]/div/ul[2]/li[1]/button")
-        element.click()
+    element=driver.find_element_by_xpath("/html/body/div[2]/div/ul[2]/li[1]/button")
+    element.click()
 
-        #关闭浏览器
-        sleep(3)
-        driver.quit()
-        print("success")
-    except Exception as e:
-        print("fail")
-        pass
+    #关闭浏览器
+    sleep(3)
+    driver.quit()
+    print("success")
+#     except Exception as e:
+#         print("fail")
+#         pass
 
 checkin(username = os.environ["USERNAME"],
         password = os.environ["PASSWORD"])
